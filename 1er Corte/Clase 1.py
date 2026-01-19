e0325bc21b477a0665c81ae6a6afb19acd63d868
ElArvizu <angelurielarvizumartinez15@gmail.com>
2026-01-19 07:10:07 -0600

    Add files via upload

---
+++ 1er Corte/Clase 1.py
@@ -0,0 +1,18 @@
+print ("ejercicio de la clase 1 ")
+A=10 # valor de A
+B=3 # Valor de B 
+C=A+B #SUMA 
+D=A-B #RESTA 
+E=A*B #MULTIPLICACION 
+F=A/B #DIVICION
+G=A%B #DA EL RESIDUO DE LA DIVCION 
+H=A//B #DA LA PARTE ENTERA DE LA DIVICION 
+I=A**B #A ELEVADA A B 
+
+print("el resultado de la suma es",C)
+print("el resultado de la resta es",D)
+print("el resultado de la multiplicacion es",E)
+print("el resultado de la divicion es",F)
+print("el residuo de la divicion es",G)
+print("es la parte entera de la divicion",H)
+print("A evelevado a B",I)
---
+++ 1er Corte/clase 2.py
@@ -0,0 +1,35 @@
+print("EJERCICIO CLASE 2")# imprime eso 
+A=10 #asignacion de variables 
+B=6
+
+Z=5# NUEVAS ASIGNACIONES O COMPARADORES 
+X=9
+
+C=A>B#ASIGNA UNA COMPARACION 
+D= A<B#ASIGNA UNA COMPARACION
+E=A>=B#ASIGNA UNA COMPARACION
+F=A<=B#ASIGNA UNA COMPARACION
+G=A==B#ASIGNA UNA COMPARACION
+H=A!=B#ASIGNA UNA COMPARACION
+
+
+print("el valor de A es mayor a B?:",C)# imprime eso 
+print("el valor de A es menor que B?:",D)# imprime eso 
+print("el valor de A es mayor o igual que B?:",E)# imprime eso 
+print("el valor de A es menor o igual que B?:",F)# imprime eso 
+print("el valor de A es igual que B?:",G)# imprime eso 
+print("el valor de A es indiferente o igual que B?:",H)# imprime eso 
+
+print(A>B and X>=Z)
+print(Z!=A or B==X)
+print(not(A==Z))
+
+print(A>B and X<Z) #Muestra si ambas se cumplen 
+print(A==Z or X>=B) #Muestra su akguna se cumple 
+print(not(A==X)) #Muestra lo contraio de la comparacion 
+
+print(type("JAJA"))
+print(type(8))
+print(type(5.45))
+print(type(5>3))
+print(type(5+3j))
