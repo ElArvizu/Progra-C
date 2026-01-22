d48512ad305e5f0fe74672664cd68f9dca71b3dc
ElArvizu <angelurielarvizumartinez15@gmail.com>
2026-01-22 07:43:10 -0600

    Update tarea de if.py

--- 1er Corte/tarea de if.py
+++ 1er Corte/tarea de if.py
@@ -1,35 +1,35 @@
-print("tarea")
-A=10
-B=3
-X=6
-Z=9
+print("tarea") #imprime el nombre de la actvidad 
+A=10 #define el valor de A 
+B=3 #define el valor de B
+X=6 #define el valor de X 
+Z=9 #define el valor de Z 
 
-if A>B:
-    print("A es mayor a B")
-else:
-    print("A no es mayor a B")
+if A>B: # define si A es mayor que B
+    print("A es mayor a B") # IMPRIME EL RESULTADO DE IF
+else: # condicional "Y SI NO?"
+    print("A no es mayor a B") #IMPRIME EL RESULTADO DE "ELSE"
 
-if X>Z:
-    print("X es nmayor que z")
-else:
-    print("X no es mayor que Z")
+if X>Z: # define si X es mayor que Z
+    print("X es nmayor que z") #IMPRIME EL RESULTADO DE IF
+else: #condicional "Y SI NO?"
+    print("X no es mayor que Z") #IMPRIME EL RESULTADO DE "ELSE"
 
-if A>X:
-    print("A es mayor que X")
-else:
-    print("A no es mayor que X")
+if A>X: # define si A es mayor que X
+    print("A es mayor que X") #IMPRIME EL RESULTADO DE IF
+else: #condicional "Y SI NO?"
+    print("A no es mayor que X") #IMPRIME EL RESULTADO DE "ELSE"
 
-if A>Z:
-    print("A es mayor que Z")
-else:
-    print("A no es menor que Z")
+if A>Z: # define si A es mayor que Z
+    print("A es mayor que Z") #IMPRIME EL RESULTADO DE IF
+else: #condicional "Y SI NO?"
+    print("A no es menor que Z") #IMPRIME EL RESULTADO DE "ELSE"
 
-if B>X:
-    print("B es mayor que X")
-else:
-    print("B no es mayor que X")
+if B>X: # define si B es mayor que X
+    print("B es mayor que X") #IMPRIME EL RESULTADO DE IF
+else: #condicional "Y SI NO?"
+    print("B no es mayor que X") #IMPRIME EL RESULTADO DE "ELSE"
 
-if B>Z:
-    print("B es mayor que Z")
-else:
-    print("B no es mayor que Z")
+if B>Z: # define si B es mayor que Z
+    print("B es mayor que Z") #IMPRIME EL RESULTADO DE IF
+else: #condicional "Y SI NO?"
+    print("B no es mayor que Z") #IMPRIME EL RESULTADO DE "ELSE"
